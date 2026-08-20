d36b1bba68e10ff61def15d3bc223417e3c61fb7
Hawkar-usls <hawkarlol@gmail.com>
2026-08-21 01:51:12 +0300

    Export Aura-Habitat spiral runtime components

--- src/janus_spi/__init__.py
+++ src/janus_spi/__init__.py
@@ -1,11 +1,17 @@
+from .aura_habitat_spiral import AuraPeerAdapter, DialogueLedger, HabitatMirror, SpiralDialogueEngine, SpiralTurn
 from .core import Forecast, JanusSPICore, Ledger, SemanticEvent, SemanticMemory
 from .github_observer import GitHubObserver
 
 __all__ = [
+    "AuraPeerAdapter",
+    "DialogueLedger",
     "Forecast",
     "GitHubObserver",
+    "HabitatMirror",
     "JanusSPICore",
     "Ledger",
     "SemanticEvent",
     "SemanticMemory",
+    "SpiralDialogueEngine",
+    "SpiralTurn",
 ]
