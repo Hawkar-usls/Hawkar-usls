d3c5e606c54d830bf3f4208c8b8c7ab505b49b60
Hawkar-usls <hawkarlol@gmail.com>
2026-08-21 01:27:46 +0300

    Add JANUS-SPI package entry

---
+++ src/janus_spi/__init__.py
@@ -0,0 +1,11 @@
+from .core import Forecast, JanusSPICore, Ledger, SemanticEvent, SemanticMemory
+from .github_observer import GitHubObserver
+
+__all__ = [
+    "Forecast",
+    "GitHubObserver",
+    "JanusSPICore",
+    "Ledger",
+    "SemanticEvent",
+    "SemanticMemory",
+]
