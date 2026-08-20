875805f60a93befb04c225d123375b2adc25d016
Hawkar-usls <hawkarlol@gmail.com>
2026-08-21 01:55:10 +0300

    Export Habitat event bus

--- src/janus_spi/__init__.py
+++ src/janus_spi/__init__.py
@@ -1,12 +1,14 @@
 from .aura_habitat_spiral import AuraPeerAdapter, DialogueLedger, HabitatMirror, SpiralDialogueEngine, SpiralTurn
 from .core import Forecast, JanusSPICore, Ledger, SemanticEvent, SemanticMemory
 from .github_observer import GitHubObserver
+from .habitat_bus import HabitatEventBus
 
 __all__ = [
     "AuraPeerAdapter",
     "DialogueLedger",
     "Forecast",
     "GitHubObserver",
+    "HabitatEventBus",
     "HabitatMirror",
     "JanusSPICore",
     "Ledger",
