6ae3aa80c9ad6b8af0a19855bb8ba74ebeae5e98
Hawkar-usls <hawkarlol@gmail.com>
2026-08-29 20:32:14 +0300

    Activator v0.3: expose dispatch broker from janus_spi

--- src/janus_spi/__init__.py
+++ src/janus_spi/__init__.py
@@ -1,6 +1,7 @@
 from .activator import ActivationEvent, ActivationLedger, JanusActivator
 from .aura_habitat_spiral import AuraPeerAdapter, DialogueLedger, HabitatMirror, SpiralDialogueEngine, SpiralTurn
 from .core import Forecast, JanusSPICore, Ledger, SemanticEvent, SemanticMemory
+from .dispatch import DispatchLedger, JanusDispatchBroker, verify_dispatch_packet, verify_sealed_receipt
 from .github_observer import GitHubObserver
 from .habitat_bus import HabitatEventBus
 
@@ -9,15 +10,19 @@ __all__ = [
     "ActivationLedger",
     "AuraPeerAdapter",
     "DialogueLedger",
+    "DispatchLedger",
     "Forecast",
     "GitHubObserver",
     "HabitatEventBus",
     "HabitatMirror",
     "JanusActivator",
+    "JanusDispatchBroker",
     "JanusSPICore",
     "Ledger",
     "SemanticEvent",
     "SemanticMemory",
     "SpiralDialogueEngine",
     "SpiralTurn",
+    "verify_dispatch_packet",
+    "verify_sealed_receipt",
 ]
