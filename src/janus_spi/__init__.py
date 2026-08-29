356e4fca467b039d70e1db406f090215a321584c
Hawkar-usls <hawkarlol@gmail.com>
2026-08-29 20:05:10 +0300

    activator: export root activation kernel

--- src/janus_spi/__init__.py
+++ src/janus_spi/__init__.py
@@ -1,15 +1,19 @@
+from .activator import ActivationEvent, ActivationLedger, JanusActivator
 from .aura_habitat_spiral import AuraPeerAdapter, DialogueLedger, HabitatMirror, SpiralDialogueEngine, SpiralTurn
 from .core import Forecast, JanusSPICore, Ledger, SemanticEvent, SemanticMemory
 from .github_observer import GitHubObserver
 from .habitat_bus import HabitatEventBus
 
 __all__ = [
+    "ActivationEvent",
+    "ActivationLedger",
     "AuraPeerAdapter",
     "DialogueLedger",
     "Forecast",
     "GitHubObserver",
     "HabitatEventBus",
     "HabitatMirror",
+    "JanusActivator",
     "JanusSPICore",
     "Ledger",
     "SemanticEvent",
